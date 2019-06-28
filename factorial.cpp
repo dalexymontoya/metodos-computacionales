3e90aded0c0175b7142df7113904971146938b5d
dalexymontoya <52118414+dalexymontoya@users.noreply.github.com>
2019-06-28 11:55:18 -0500

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int factorial =0;
+int main()
+{
+    for(int i=1; i=7; i++)
+    {
+        factorial*= i;
+        cout<<"el factorial de n es:"<< factorial;
+    }
+    return 0;
+}
